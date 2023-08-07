4c1b32afb706f23c950643f049cc9767b7fb0399
Aswin <aswinasokofficial@gmail.com>
2023-08-08 00:57:18 +0530

    feat(reviews):lang-translator-added

--- src/components/Testimonial/Slider.jsx
+++ src/components/Testimonial/Slider.jsx
@@ -1,16 +1,11 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
+import React from "react";
+import { useTranslation } from "react-i18next";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
 import "./Slider.css";
-
-// import required modules
-import { Navigation, Pagination } from "swiper/modules";
 import img1 from "../../assets/testimonial/img1.png";
 import img2 from "../../assets/testimonial/img2.png";
 import img3 from "../../assets/testimonial/img3.png";
@@ -18,43 +13,41 @@ import img4 from "../../assets/testimonial/img4.png";
 import img5 from "../../assets/testimonial/img5.png";
 
 const Slider = () => {
+  const { t } = useTranslation(["Slider"]);
+  
   const data = [
     {
-      content:
-        "Working on social projects during my internship was a powerful journey of empathy and personal growth.",
+      content: t("testimonial1_content"),
       name: "Saran S",
-      designation: "MA Public Policy & Governance, TISS Hyderabad",
+      designation: t("testimonial1_designation"),
       image: img1,
     },
     {
-      content:
-        "My internship at UNNATHI was a life-changing experience that has made me a more passionate advocate for social policies.",
+      content: t("testimonial2_content"),
       name: "Anamika Prajin",
-      designation: "MA Public Policy & Governance, TISS Hyderabad",
+      designation: t("testimonial2_designation"),
       image: img2,
     },
     {
-      content:
-        "It was an honour to have worked with a talented and creative team -collectively contributing towards Unnathi's mission",
+      content: t("testimonial3_content"),
       name: "Shairin Konghay",
-      designation: "MA Public Policy & Governance,TISS Hyderabad",
+      designation: t("testimonial3_designation"),
       image: img3,
     },
     {
-      content:
-        "The team's boundless commitment and extraordinary efforts have left an indelible mark on me.",
+      content: t("testimonial4_content"),
       name: "Athira K.G.",
-      designation: "MA Public Policy & Governance, TISS Hyderabad",
+      designation: t("testimonial4_designation"),
       image: img4,
     },
     {
-      content:
-        "My internship at UNNATHI was a life-changing experience that has made me a more passionate advocate for social policies.",
+      content: t("testimonial5_content"),
       name: "Supratim Bhattacharjee",
-      designation: "MA Public Policy & Governance, TISS Hyderabad",
+      designation: t("testimonial5_designation"),
       image: img5,
     },
   ];
+
   return (
     <div className="slider">
       <Swiper
@@ -65,24 +58,22 @@ const Slider = () => {
         modules={[Navigation, Pagination]}
         className="mySwiper"
       >
-        {" "}
         {data.map(({ image, designation, name, content }, i) => (
-          <SwiperSlide>
-            <div class="card">
+          <SwiperSlide key={i}>
+            <div className="card">
               <p>"{content}"</p>
               <div className="card-name-img">
                 <div className="person-name">
                   <h2>{name}</h2>
                   <p>{designation}</p>
                 </div>
-
                 <div>
-                  <img src={image} />
+                  <img src={image} alt={name} />
                 </div>
               </div>
-            </div>{" "}
+            </div>
           </SwiperSlide>
-        ))}{" "}
+        ))}
       </Swiper>
     </div>
   );
