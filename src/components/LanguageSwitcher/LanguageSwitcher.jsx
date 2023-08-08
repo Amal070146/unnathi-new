f4ddf635c370bdb0c3bca5d232e14ce3173e80ab
Aswin <aswinasokofficial@gmail.com>
2023-08-09 01:24:03 +0530

    removed-url-lang

--- src/components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -9,18 +9,17 @@ function LanguageSwitcher() {
     i18n.changeLanguage(event.target.value);
   };
 
-  //Add the value of the language to the query parameter lang in the URL
-  const urlParams = new URLSearchParams(window.location.search);
-  urlParams.set("lang", i18n.language);
-  const newUrlParams = urlParams.toString();
-  const newUrl =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    window.location.pathname +
-    "?" +
-    newUrlParams;
-  window.history.pushState({ path: newUrl }, "", newUrl);
+  // const urlParams = new URLSearchParams(window.location.search);
+  // urlParams.set("lang", i18n.language);
+  // const newUrlParams = urlParams.toString();
+  // const newUrl =
+  //   window.location.protocol +
+  //   "//" +
+  //   window.location.host +
+  //   window.location.pathname +
+  //   "?" +
+  //   newUrlParams;
+  // window.history.pushState({ path: newUrl }, "", newUrl);
 
   return (
     <div className="LanguageSwitcher-wrapper">
