ec6d182998571061bb9f381fb0d23d9ca6ce9c0a
Amal C P <acp070146@gmail.com>
2023-08-08 13:31:56 +0530

    banner set for internship page

--- src/pages/JoinUs-pages/Internship/Internships.jsx
+++ src/pages/JoinUs-pages/Internship/Internships.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Internship.css";
+import banner from "../../../assets/JoinUspage/yellow-banner-img/internship.png";
 import Navbar from "../../../components/Navbar/Navbar";
 import Footer from "../../../components/Footer/Footer";
 import { useTranslation } from "react-i18next";
@@ -47,7 +48,7 @@ const Internship = () => {
           <p>{t("dddText")}</p>
         </div>
         {/* card section starts*/}
-        <div className="internship-page-card-wrapper">
+        <div className="internship-page-card-wrapper" id="about-internship">
           <h1>
             <span> {t("whyChoose")}</span> {t("unnathiInternship")}
           </h1>
@@ -354,7 +355,7 @@ const Internship = () => {
           <h1>
             <span>{t("who")}</span> {t("lookingFor")}
           </h1>
-          <div>
+          <div className="internship-numbered-descriptions-content-wrapper">
             <div>
               <h2>1</h2>
               <div>
@@ -385,6 +386,44 @@ const Internship = () => {
             </div>
           </div>
         </div>
+
+        {/* yellow banner start */}
+        <div className="internship-yellow-banner-wrapper">
+          <div className="internship-yellow-banner-content-wrapper">
+            <h1>
+              The Unnathi <span>Promise</span>
+            </h1>
+            <p>
+              Joining the Unnathi Internship Program is more than just a
+              professional stint. It's a commitment to growth, both personal and
+              professional. It's a journey where every day offers new learning,
+              new challenges, and countless moments of fulfillment. Here, you
+              won't just be working; you'll be evolving, influencing, and
+              inspiring.
+            </p>
+            {/* <a href="">
+              <p>Submit</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="23"
+                height="20"
+                viewBox="0 0 29 23"
+                fill="none"
+              >
+                <path
+                  d="M27.0839 12.5299C27.3655 12.2482 27.5237 11.8662 27.5237 11.4679C27.5237 11.0695 27.3655 10.6875 27.0839 10.4058L18.586 1.908C18.4475 1.76453 18.2817 1.65009 18.0984 1.57136C17.9152 1.49263 17.7181 1.45119 17.5186 1.44946C17.3191 1.44773 17.1213 1.48573 16.9367 1.56126C16.7521 1.6368 16.5844 1.74834 16.4433 1.88938C16.3023 2.03042 16.1908 2.19814 16.1152 2.38276C16.0397 2.56737 16.0017 2.76518 16.0034 2.96463C16.0052 3.16409 16.0466 3.36121 16.1253 3.54448C16.204 3.72775 16.3185 3.89351 16.462 4.03208L22.3956 9.96568L2.50222 9.96568C2.10382 9.96568 1.72173 10.1239 1.44002 10.4057C1.15831 10.6874 1.00004 11.0695 1.00004 11.4679C1.00004 11.8663 1.15831 12.2483 1.44002 12.5301C1.72173 12.8118 2.10382 12.97 2.50222 12.97L22.3956 12.97L16.462 18.9036C16.1883 19.187 16.0369 19.5664 16.0403 19.9603C16.0438 20.3541 16.2017 20.7309 16.4803 21.0094C16.7588 21.2879 17.1355 21.4459 17.5294 21.4493C17.9233 21.4528 18.3027 21.3014 18.586 21.0277L27.0839 12.5299Z"
+                  fill="black"
+                  stroke="white"
+                  stroke-width="1.2"
+                />
+              </svg>
+            </a> */}
+          </div>
+          <div className="internship-yellow-banner-image-wrapper">
+            <img src={banner} alt="" />
+          </div>
+        </div>
+        {/* yellow banner ends */}
         {/* eligibility criteria */}
         <div className="volunteering-eligibility-selection">
           <div>
