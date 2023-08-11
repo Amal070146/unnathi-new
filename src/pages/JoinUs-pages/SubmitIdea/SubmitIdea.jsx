53875d26f267fa1a30df73142f9efd4a6667be60
Amal C P <acp070146@gmail.com>
2023-08-11 22:30:33 +0530

    submit idea page full completed

--- src/pages/JoinUs-pages/SubmitIdea/SubmitIdea.jsx
+++ src/pages/JoinUs-pages/SubmitIdea/SubmitIdea.jsx
@@ -50,14 +50,15 @@ const SubmitIdea = () => {
           <p>{t("collectiveImpactText")}</p>
         </div>
         {/* about of submitidea */}
+        {/* submit idea card */}
         <div id="about-submitidea" className="about-volunteering">
           <div className="about-volunteering-heading">
             <h1>
               <span>{t("whyShare")}</span>&nbsp;{t("withUnnathi")}
             </h1>
           </div>
-          <div className="volunteer-card-wrapper">
-            <div className="volunteer-card-individual">
+          <div className="submitidea-card-wrapper">
+            <div className="submitidea-card-individual card1">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="108"
@@ -97,7 +98,7 @@ const SubmitIdea = () => {
               <h4>{t("platformForChange")}</h4>
               <p>{t("platformForChangeText")}</p>
             </div>
-            <div className="volunteer-card-individual">
+            <div className="submitidea-card-individual card2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="141"
@@ -192,7 +193,7 @@ const SubmitIdea = () => {
               <p>{t("collaborativeGrowthText")}</p>
             </div>
 
-            <div className="volunteer-card-individual">
+            <div className="submitidea-card-individual card3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="191"
@@ -398,7 +399,8 @@ const SubmitIdea = () => {
         <div className="internship-yellow-banner-wrapper">
           <div className="internship-yellow-banner-content-wrapper">
             <h1>
-              <span>{t("beTheSpark")}</span>{t("ofTransformation")}
+              <span>{t("beTheSpark")}</span>
+              {t("ofTransformation")}
             </h1>
             <p>{t("transformationText")}</p>
             <a href="">
