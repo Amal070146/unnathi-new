572f6d0614f16df50c677a6d8029fe3a01aa71de
Amal C P <acp070146@gmail.com>
2023-10-16 21:18:45 +0530

    section added

---
+++ src/pages/JoinUs-pages/ProjectFunds/ListProject.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListProject = (value) => {
+    console.log(value)
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default ListProject
--- src/pages/JoinUs-pages/ProjectFunds/ProjectsFund.jsx
+++ src/pages/JoinUs-pages/ProjectFunds/ProjectsFund.jsx
@@ -11,6 +11,7 @@ import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import ListProject from "./ListProject";
 AOS.init();
 const ProjectsFund = () => {
   const { t } = useTranslation(["ProjectsFund"]);
@@ -346,6 +347,28 @@ const ProjectsFund = () => {
           <h1>
             <span>Explore</span>&nbsp;our Projects
           </h1>
+          <div>
+            <ListProject
+              title="This is project"
+              knowMore="https://www.google.com"
+              SupportUs="1"
+            />
+            <ListProject
+              title="This is project"
+              knowMore="https://www.google.com"
+              SupportUs="1"
+            />{" "}
+            <ListProject
+              title="This is project"
+              knowMore="https://www.google.com"
+              SupportUs="1"
+            />{" "}
+            <ListProject
+              title="This is project"
+              knowMore="https://www.google.com"
+              SupportUs="1"
+            />
+          </div>
         </div>
 
         {/*  timeline start */}
