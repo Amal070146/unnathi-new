29d4a81cf79ebb64bc3d8641fac1150105656452
Aswin Asok <aswinasokofficial@gmail.com>
2023-10-27 19:45:39 +0530

    feat(internship-tenders):form-added-tenders-added

--- src/pages/NavbarPages/Tender/Tender.jsx
+++ src/pages/NavbarPages/Tender/Tender.jsx
@@ -7,12 +7,18 @@ import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 
 const data = [
-  // {
-  //   id: 1,
-  //   title:
-  //     "Quotation Invited for GPR Survey for mapping underground utilities @ Peroorkada in Thiruvananthapuram District",
-  //   link: "https://web.rbdck.com/wp-content/uploads/2023/09/GPR-Survey-quotation-final.pdf",
-  // },
+  {
+    id: 1,
+    title:
+      "Expression of interest (EOI) for selection of the establishment of a project division that will function on a self sustainable financial self-dependent model under Unnathi.",
+    link: "https://drive.google.com/file/d/16fKRcAju73T0cRcS118s3EBR4LlEHhzK/view",
+  },
+  {
+    id: 2,
+    title:
+      "Expression of Interest (EOI) for selection of consultants for the preparation of detailed project report (DPR) as per the guidelines of Government of India & Kerala in consultation with Unnathi",
+    link: "https://drive.google.com/file/d/1BTZfc9caaPItAZRIT3hXobIbwf-bW9TV/view",
+  },
 ];
 
 const Tender = () => {
