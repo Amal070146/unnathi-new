b5772662eab9b22640c078a8c206b34b4731145c
Aswin Asok <aswinasokofficial@gmail.com>
2024-02-27 18:20:27 +0530

    feat(tender):new-tender-added

--- src/pages/NavbarPages/Tender/Tender.jsx
+++ src/pages/NavbarPages/Tender/Tender.jsx
@@ -7,53 +7,54 @@ import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 
 const data = [
-  {
-    id: 1,
-    title:
-      "Expression of interest (EOI) for selection of the establishment of a project division that will function on a self sustainable financial self-dependent model under Unnathi.",
-    link: "https://drive.google.com/file/d/16fKRcAju73T0cRcS118s3EBR4LlEHhzK/view",
-  },
-  {
-    id: 2,
-    title:
-      "Expression of Interest (EOI) for selection of consultants for the preparation of detailed project report (DPR) as per the guidelines of Government of India & Kerala in consultation with Unnathi",
-    link: "https://drive.google.com/file/d/1BTZfc9caaPItAZRIT3hXobIbwf-bW9TV/view",
-  },
-  {
-    id: 3,
-    title:
-      "TIME EXTENSION - Expression of Interest (EOI) for selection of consultants for the preparation of detailed project report (DPR) as per the guidelines of Government of India & Kerala in consultation With Unnathi",
-    link: "https://drive.google.com/file/d/159b5-nEITNVhnfbZSFNZgMnV5-AlRz7B/view?usp=drivesdk",
-  },
-  {
-    id: 4,
-    title:
-      "TIME EXTENSION - Expression of interest (EOI) for selection of the establishment of a project division that will function on a self sustainable financial self-dependent model under Unnathi.",
-    link: "https://drive.google.com/file/d/1epNg7AoM5d9KE_uzbdpsSyYNnRYESeUg/view?usp=drivesdk",
-  }
+    {
+        id: 1,
+        title: "Expression of interest (EOI) for selection of the establishment of a project division that will function on a self sustainable financial self-dependent model under Unnathi.",
+        link: "https://drive.google.com/file/d/16fKRcAju73T0cRcS118s3EBR4LlEHhzK/view",
+    },
+    {
+        id: 2,
+        title: "Expression of Interest (EOI) for selection of consultants for the preparation of detailed project report (DPR) as per the guidelines of Government of India & Kerala in consultation with Unnathi",
+        link: "https://drive.google.com/file/d/1BTZfc9caaPItAZRIT3hXobIbwf-bW9TV/view",
+    },
+    {
+        id: 3,
+        title: "TIME EXTENSION - Expression of Interest (EOI) for selection of consultants for the preparation of detailed project report (DPR) as per the guidelines of Government of India & Kerala in consultation With Unnathi",
+        link: "https://drive.google.com/file/d/159b5-nEITNVhnfbZSFNZgMnV5-AlRz7B/view?usp=drivesdk",
+    },
+    {
+        id: 4,
+        title: "TIME EXTENSION - Expression of interest (EOI) for selection of the establishment of a project division that will function on a self sustainable financial self-dependent model under Unnathi.",
+        link: "https://drive.google.com/file/d/1epNg7AoM5d9KE_uzbdpsSyYNnRYESeUg/view?usp=drivesdk",
+    },
+    {
+        id: 5,
+        title: "EXPRESSION OF INTEREST (EOI) FOR SELECTION OF THE ESTABLISHMENT OF A PROJECT DIVISION THAT WILL FUNCTION ON A SELF SUSTAINABLE FINANCIAL SELF-DEPENDENT MODEL UNDER UNNATHI.",
+        link: "https://drive.google.com/file/d/1ng5uam-lI9cUOPuKhUjo4Z3CGk9rtiok/view"
+    },
 ];
 
 const Tender = () => {
-  return (
-    <div className={styles.outerWrapper}>
-      <Navbar />
-      <div className={styles.tenderWrapper}>
-        <div>
-          {data.map(({ id, title, link }) => {
-            return (
-              <a href={link} target="_blank">
-                {title}
-              </a>
-            );
-          })}
+    return (
+        <div className={styles.outerWrapper}>
+            <Navbar />
+            <div className={styles.tenderWrapper}>
+                <div>
+                    {data.reverse().map(({ id, title, link }) => {
+                      return (
+                        <a href={link} target="_blank">
+                          {title}
+                        </a>
+                      );
+                    })}
+                </div>
+            </div>
+            <div className={styles.footer}>
+                {" "}
+                <Footer />
+            </div>
         </div>
-      </div>
-      <div className={styles.footer}>
-        {" "}
-        <Footer />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Tender;
